9ff7ff8b9afd1b4ae95528afa9a9f9cfe06f4082
Mr. Angry Pants <abricker@mac.com>
2019-02-08 20:41:25 -0600

    Stand up Store in app via AppDelegate

--- FeelingMeterIOS/AppDelegate.swift
+++ FeelingMeterIOS/AppDelegate.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import ReSwift
+
+let mainStore = Store<MainState>(
+    reducer: mainReducer,
+    state: nil
+)
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
